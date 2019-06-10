56a4d21fc00555d5ada25e4d2a07a76e11c83ce5
jneaton922 <jneaton922@gmail.com>
2019-06-10 14:25:41 -0400

    Most of MTAlpha, started DLB, untested

--- A2/DLBNode.java
+++ A2/DLBNode.java
@@ -9,15 +9,100 @@ public class DLBNode<V> implements TrieNodeInt<V>
 {
     protected Nodelet front;
     protected int degree;
-	protected V val;
-	
+	  protected V val;
+
     protected class Nodelet
     {
     	protected char cval;
     	protected Nodelet rightSib;
     	protected TrieNodeInt<V> child;
+      protected Nodelet(){};
     }
-    		
+
 	// You must supply the methods for this class.  See TrieNodeInt.java for the
 	// interface specifications.  You will also need a constructor or two.
+
+  public DLBNode(V data){
+    degree = 0;
+    val = data;
+    front = new Nodelet();
+  }
+  public DLBNode(){
+    val = null;
+    degree = 0;
+    front = new Nodelet();
+
+  }
+
+
+
+  // Return the next node in the trie corresponding to character
+  // c in the current node, or null if there is not next node for
+  // that character.
+  public TrieNodeInt<V> getNextNode(char c){
+    Nodelet nodelet = front;
+    while ((nodelet.cval != c) && (nodelet != null)){
+      nodelet = nodelet.rightSib
+    }
+    if (nodelet == null) return null;
+    return nodelet.child
+
+  }
+
+  // Set the next node in the trie corresponding to character char
+  // to the argument node.  If the node at that position was previously
+  // null, increase the degree of this node by one (since it is now
+  // branching by one more link).
+  public void setNextNode(char c, TrieNodeInt<V> node){
+    Nodelet nodelet = front;
+    while(nodelet.cval > c && nodelet != null) nodelet = nodelet.rightSib;
+    if (nodelet.child == null) {
+      degree ++;
+      nodelet.cval = c;
+    }
+    nodelet.child = node;
+  }
+
+  // Return the data at the current node (or null if there is no data)
+  public V getData(){
+    return val;
+  }
+
+  // Set the data at the current node to the data argument
+  public void setData(V data){
+    val = data;
+  }
+
+  // Return the degree of the current node.  This corresponds to the
+  // number of children that this node has.
+  public int getDegree(){
+    return degree;
+  }
+
+  // Return the approximate size in bytes of the current node.  This is
+  // a very rough approximation based on the following:
+  // 1) Assume each reference in a node will use 4 bytes (whether it is
+  //    used or it is null)
+  // 2) Assume any primitive type is its specified size (see Java reference
+  //    for primitive type sizes in bytes)
+  // Note that the actual size of the node is implementation dependent and
+  // is not specified in the Java language.  There are tools to give a better
+  // approximation of this value but for our purposes, this approximation is
+  // fine.
+  public int getSize(){
+    // each node has (degree) nodelets
+    // an int for degree
+  }
+
+  // Return an Iterable collection of the references to all of the children
+  // of this node.  Do not put any null references into this result.  The
+  // order of the children as stored in the TrieNodeInt<V> node must be
+  // maintained in the returned Iterable.  The easiest way to do this is to
+  // put all of the references into a Queue and to return the Queue (since a
+  // Queue implements Iterable and maintains the order of the children).
+  // This method will allow us to access all of the children of a node without
+  // having to know how the node is actually implemented.
+  public Iterable<TrieNodeInt<V>> children(){
+
+  }
 }
--- A2/MTAlphaNode.java
+++ A2/MTAlphaNode.java
@@ -7,17 +7,93 @@
 package TriePackage;
 
 import java.util.*;
+import java.lang.*;
+
 public class MTAlphaNode<V> implements TrieNodeInt<V>
 {
-	private static final int R = 26;	// 26 letters in
-										// alphabet
-
-    protected V val;
-    protected TrieNodeInt<V> [] next;
+	private static final int R = 26;						// 26 letters in
+																	  					// alphabet
+	private static final int INDEX_OFFSET = 65; // integer value for 'a', offset
+																							// to map a-z into 0-25 indices
+  protected V val;
+  protected TrieNodeInt<V> [] next;
 	protected int degree;
 
-	// You must supply the methods for this class.  See TrieNodeInt.java
-	// for the specifications.  See also handout MTNode.java for a
-	// partial implementation.  Don't forget to include the conversion
-	// constructor (passing a DLBNode<V> as an argument).
+	// conversion constructor
+	// create an MTAlphaNode from an existing DLBNode
+	public MTAlphaNode(DLBNode<V> dlb_node){
+			val = dlb_node.val;
+			degree = dlb_node.degree;
+			next = (TrieNodeInt<V> []) new TrieNodeInt<?>[R];
+
+ 			Nodelet nodelet = dlb_node.front;
+			while (nodelet != null){
+				next[nodelet.cval - INDEX_OFFSET] = nodelet.child
+				nodelet = nodelet.rightSib
+			}
+	}
+
+	public MTAlphaNode(V data)
+	{
+		val = data;
+		degree = 0;
+		next = (TrieNodeInt<V> []) new TrieNodeInt<?>[R];
+	}
+	public MTAlphaNode()
+	{
+		val = null;
+		degree = 0;
+		next = (TrieNodeInt<V> []) new TrieNodeInt<?>[R];
+	}
+
+	// Return the reference corresponding to character c
+	public TrieNodeInt<V> getNextNode(char c)
+	{
+		return next[c-INDEX_OFFSET];
+	}
+
+	// Assign the argument node to the location corresponding
+	// to character c.  If the reference had been null we
+	// increase the degree of the current node by one.
+	// IndexOutOfBoundsException if c not in a-z
+	public void setNextNode(char c, TrieNodeInt<V> node) throws IndexOutOfBoundsException
+	{
+		if (next[c-INDEX_OFFSET] == null){
+			degree++;
+		}
+		next[c-INDEX_OFFSET] = node;
+	}
+
+	// Return the value of this node
+	public V getData()
+	{
+		return val;
+	}
+
+	// Set the value of this node
+	public void setData(V data)
+	{
+		val = data;
+	}
+
+	// Return the degree of this node
+	public int getDegree()
+	{
+		return degree;
+	}
+
+	// Return the approximate size in bytes of the current node.
+	public int getSize(){
+		int reference_bytes = 4;
+		int int_bytes = 4;
+		int refs = R*reference_bytes
+
+		// Each node has the 26-reference array
+		// and an int for degree
+		return refs + int_bytes;
+	}
+
+	public Iterable<TrieNodeInt<V>> children(){
+
+	}
 }
